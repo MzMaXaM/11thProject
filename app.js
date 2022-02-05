e7fa69917e64048a4bf503a493408f7affa2f202
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-05 20:52:54 +0000

    managed the routing

--- app.js
+++ app.js
@@ -2,12 +2,30 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
+app.use(express.static('public'))
+
 app.get('/', (req, res)=>{
-  res.send('<h1>Hello World!</h1>')
+  const filePath = path.join(__dirname, 'views', 'index.html')
+  res.sendFile(filePath)
 })
 
 app.get('/restaurants', (req, res)=>{
   const filePath = path.join(__dirname, 'views', 'restaurants.html')
+  res.sendFile(filePath)
+})
+
+app.get('/about', (req, res)=>{
+  const filePath = path.join(__dirname, 'views', 'about.html')
+  res.sendFile(filePath)
+})
+
+app.get('/confirm', (req, res)=>{
+  const filePath = path.join(__dirname, 'views', 'confirm.html')
+  res.sendFile(filePath)
+})
+
+app.get('/recommend', (req, res)=>{
+  const filePath = path.join(__dirname, 'views', 'recommend.html')
   res.sendFile(filePath)
 })
 
