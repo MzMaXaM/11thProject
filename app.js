1413af953b4af90f6d3b34ff3d6578491176ea4b
MzMaXaM <55605842+MzMaXaM@users.noreply.github.com>
2022-02-09 21:44:19 +0000

    query selectors!!!

--- app.js
+++ app.js
@@ -17,17 +17,29 @@ app.get('/', (req, res) => {
 })
 
 app.get('/restaurants', (req, res) => {
+  let order = req.query.order
+  order == 'asc' ? order = 'dsc' : order = 'asc'
   const storedRestaurants = util.getRestaurants()
+
+  storedRestaurants.sort((res1, res2) => {
+    if (order == 'asc' && res1.name > res2.name) {
+      return 1
+    } else if (order == 'dsc' && res2.name > res1.name) {
+      return 1
+    }
+    return -1
+  })
 
   res.render('restaurants', {
     numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants
+    restaurants: storedRestaurants,
+    order: order
   })
 })
 
 app.get('/restaurants/:id', (req, res) => {
   const restaurantId = req.params.id
- 
+
   const storedRestaurants = util.getRestaurants()
 
   for (const restaurant of storedRestaurants) {
@@ -65,7 +77,7 @@ app.post('/recommend', (req, res) => {
   res.redirect('/confirm')
 })
 
-app.use((req, res)=>{
+app.use((req, res) => {
   res.status(404).render('404')
 })
 
